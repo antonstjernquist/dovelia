b6b8f0fa8c138fd27b36bd75a7299d73050aa169
Anton Stjernquist <antonstjernquist@gmail.com>
2020-09-06 15:32:24 +0200

    Moved build into /docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+const fs = require("fs")
+
+const outputFolder = "docs"
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, outputFolder), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(
+    path.join(__dirname, "public"),
+    path.join(__dirname, outputFolder)
+  )
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
