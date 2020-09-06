608d2ebdc3c137ecfad315c94a85ac02bda5fa99
Anton Stjernquist <antonstjernquist@gmail.com>
2020-09-06 18:16:32 +0200

    Fixed build with styled-components

--- src/components/views/LandingPage.tsx
+++ src/components/views/LandingPage.tsx
@@ -1,5 +1,5 @@
-import React from "react"
 import GlobalStyle from "../global"
+import React from "react"
 
 import styled from "styled-components"
 import Instagram from "../../assets/instagram.svg"
